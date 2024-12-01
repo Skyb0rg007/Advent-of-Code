55085cd505ba22bcac0d6393b2bde5de850e701c
ssoss <skyler.soss@gmail.com>
2024-12-01 00:59:06 -0500

    2024

---
+++ 2024/1/solution.lua
@@ -0,0 +1,51 @@
+#!/usr/bin/env lua5.4
+
+local filename = arg[1] or 'test.txt'
+
+local lhs, rhs = {}, {}
+local n = 0
+
+for line in io.lines(filename) do
+    local a, b = assert(line:match('(%d+)%s+(%d+)'))
+    n = n + 1
+    lhs[n] = assert(tonumber(a))
+    rhs[n] = assert(tonumber(b))
+end
+
+-- Part 1: Distance
+if true then
+    local lhsTagged, rhsTagged = {}, {}
+    for i = 1, n do lhsTagged[i] = { idx = i, value = lhs[i] } end
+    for i = 1, n do rhsTagged[i] = { idx = i, value = rhs[i] } end
+
+    local function comp(a, b)
+        return a.value < b.value
+    end
+
+    table.sort(lhsTagged, comp)
+    table.sort(rhsTagged, comp)
+
+    local tot = 0
+
+    for i = 1, n do
+        tot = tot + math.abs(lhsTagged[i].value - rhsTagged[i].value)
+    end
+
+    print('Part 1:', tot)
+end
+
+-- Part 2: Similarity
+if true then
+    local sim = 0
+
+    local rhsFreq = {}
+    for _, x in ipairs(rhs) do
+        rhsFreq[x] = (rhsFreq[x] or 0) + 1
+    end
+
+    for _, x in ipairs(lhs) do
+        sim = sim + x * (rhsFreq[x] or 0)
+    end
+
+    print('Part 2:', sim)
+end
