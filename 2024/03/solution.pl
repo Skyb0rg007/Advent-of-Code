4be3e46ba30fcbae1e3d29e2af18472d4355654d
ssoss <skyler.soss@gmail.com>
2024-12-03 23:36:50 -0500

    Added POD docs

--- 2024/03/solution.pl
+++ 2024/03/solution.pl
@@ -1,5 +1,25 @@
 #!/usr/bin/env perl
 
+=head1 SYNOPSIS
+
+Advent of Code 2024 Day 3 Solution
+
+=head1 USAGE
+
+C<day3> <filename>
+
+=back
+
+=head1 LICENSE
+
+This software is released under the MIT License
+
+=head1 AUTHOR
+
+Skye Soss
+
+=cut
+
 use v5.34;
 use strict;
 use warnings;
@@ -12,22 +32,15 @@ my $total2 = 0;
 my $enable = 1;
 
 while ( my $line = <$fh> ) {
-    $line =~ s/^\s+|\s+$//g;
-
-# Part 1
-    my @matches = $line =~ /mul\((\d+),(\d+)\)/g;
-    for (my $i = 0; $i < scalar @matches; $i += 2) {
-        $total1 += $matches[$i] * $matches[$i+1];
-    }
-
-    @matches = $line =~ /(?:(mul|do|don't)\((?:(\d+),(\d+))?\))/g;
+    my @matches = $line =~ /(?:(mul|do|don't)\((?:(\d+),(\d+))?\))/g;
     for (my $i = 0; $i < scalar @matches; $i += 3) {
         if ( $matches[$i] eq "do" ) {
             $enable = 1;
         } elsif ( $matches[$i] eq "don't" ) {
             $enable = 0;
-        } elsif ( $matches[$i] eq "mul" && $enable ) {
-            $total2 += $matches[$i+1] * $matches[$i+2];
+        } elsif ( $matches[$i] eq "mul" ) {
+            $total1 += $matches[$i+1] * $matches[$i+2];
+            $total2 += $matches[$i+1] * $matches[$i+2] if $enable;
         }
     }
 }
